347e533e9bec19b1ea8a0dac04200acf7b6cc75b
JuanDavid_Dev <juandavid.dev@gmail.com>
2024-08-04 19:55:52 -0500

    V.0.0.1

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const progreso = document.getElementById("progreso");
-const cancion = document.getElementById("cancion");
-const iconoControl = document.getElementById("iconoControl");
-const botonReproducirPausar = document.querySelector(".boton-reproducir-pausar");
-const botonAdelante = document.querySelector(".controles button.adelante");
-const botonAtras = document.querySelector(".controles button.atras");
-const tituloCancion = document.querySelector(".reproductor-musica h1");
-const nombreArtista = document.querySelector(".reproductor-musica p");
+const tituloCancion = document.querySelector('.reproductor-musica h1');
+const nombreArtista = document.querySelector('.reproductor-musica p');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
+
+const inconoControl = document.getElementById('iconoControl');
+const botonReproducirPausar = document.querySelector('.controles button.boton-reproducir-pausar');
+
+const botonAtras = document.querySelector('.controles button.atras');
+const botonAdelante = document.querySelector('.controles button.adelante');
 
 const canciones = [
     {
-        titulo: "A Year Ago",
-        nombre: "NEFFEX",
-        fuente: "music/A Year Ago - NEFFEX.mp3",
+        titulo:'A Year Ago',
+        nombre:'NEFFEX',
+        fuente:'music/A Year Ago - NEFFEX.mp3'
     },
     {
-        titulo: "As You Fade Away",
-        nombre: "NEFFEX",
-        fuente: "music/As You Fade Away - NEFFEX.mp3",
+        titulo:'As You Fade Away',
+        nombre:'NEFFEX',
+        fuente:'music/As You Fade Away - NEFFEX.mp3'
     },
     {
         titulo: "Catch Me If I Fall",
@@ -33,69 +36,68 @@ const canciones = [
         nombre: "NEFFEX",
         fuente: "music/Play Dead - NEFFEX.mp3",
     },
-
 ];
 
 let indiceCancionActual = 0;
 
-function actualizarInfoCancion() {
+function actualizarInfoCancion(){
     tituloCancion.textContent = canciones[indiceCancionActual].titulo;
     nombreArtista.textContent = canciones[indiceCancionActual].nombre;
     cancion.src = canciones[indiceCancionActual].fuente;
-    cancion.addEventListener("loadeddata", function () { });
-}
+    cancion.addEventListener('loadeddata',function(){});
+};
 
-cancion.addEventListener("timeupdate", function () {
-    if (!cancion.paused) {
-        progreso.value = cancion.currentTime;
-    }
-});
-
-cancion.addEventListener("loadedmetadata", function () {
+cancion.addEventListener('loadedmetadata', function(){
     progreso.max = cancion.duration;
     progreso.value = cancion.currentTime;
 });
 
-function pausarCancion() {
-    cancion.pause();
-    iconoControl.classList.remove("bi-pause-fill");
-    iconoControl.classList.add("bi-play-fill");
-}
+botonReproducirPausar.addEventListener('click', reproducirPausar);
 
-function reproducirCancion() {
-    cancion.play();
-    iconoControl.classList.add("bi-pause-fill");
-    iconoControl.classList.remove("bi-play-fill");
-}
-
-function reproducirPausar() {
-    if (cancion.paused) {
+function reproducirPausar(){
+    if(cancion.paused){
         reproducirCancion();
     } else {
         pausarCancion();
     }
+};
+
+function reproducirCancion(){
+    cancion.play();
+    inconoControl.classList.add('bi-pause-fill')
+    inconoControl.classList.remove('bi-play-fill')
 }
 
-botonReproducirPausar.addEventListener("click", reproducirPausar);
+function pausarCancion(){
+    cancion.pause();
+    inconoControl.classList.remove('bi-pause-fill')
+    inconoControl.classList.add('bi-play-fill')
+}
 
-progreso.addEventListener("input", function () {
+cancion.addEventListener('timeupdate', function(){
+    if(!cancion.paused){
+        progreso.value = cancion.currentTime;
+    }
+});
+
+progreso.addEventListener('input', function(){
     cancion.currentTime = progreso.value;
 });
 
-progreso.addEventListener("change", function () {
+// progreso.addEventListener('change', ()=>{
+//     reproducirCancion();
+// });
+
+botonAdelante.addEventListener('click', function(){
+    indiceCancionActual = (indiceCancionActual + 1) % canciones.length;
+    actualizarInfoCancion();
     reproducirCancion();
 });
 
-botonAdelante.addEventListener("click", function () {
-    indiceCancionActual = (indiceCancionActual + 1) % canciones.length;
-    actualizarInfoCancion();
-    reproducirPausar();
-});
-
-botonAtras.addEventListener("click", function () {
+botonAtras.addEventListener('click', function(){
     indiceCancionActual = (indiceCancionActual - 1 + canciones.length) % canciones.length;
     actualizarInfoCancion();
-    reproducirPausar();
+    reproducirCancion();
 });
 
 actualizarInfoCancion();
